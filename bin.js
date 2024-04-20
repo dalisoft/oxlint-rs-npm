c806751ccff2e6678782d5c3759d08811ee035d8
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-20 19:45:30 +0500

    fix(bin): improvements for binary

--- bin.js
+++ bin.js
@@ -2,10 +2,17 @@ import "./prepare.js";
 import "./package.json" assert { type: "json" };
 import fs from "node:fs/promises";
 import path from "node:path";
+import { spawnSync } from "child_process";
 
 await fs.writeFile(path.resolve(import.meta.dirname, "./package.json"), {
   ...pkg,
+  main: "prepare.js",
   bin: {
     oxlint: "oxlint",
   },
+});
+
+spawnSync("oxlint", process.argv.slice(2), {
+  shell: false,
+  stdio: "inherit",
 });
