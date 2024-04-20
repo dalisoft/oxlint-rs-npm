87b9ee0eb63fd01037d5d3c1e41a4d545525fbc0
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-20 20:03:53 +0500

    fix: improve `npx`/`bunx` compatibility

--- bin.js
+++ bin.js
@@ -1,18 +1,36 @@
+#!/usr/bin/env node
+
 import "./prepare.js";
-import "./package.json" assert { type: "json" };
+import pkg from "./package.json" assert { type: "json" };
 import fs from "node:fs/promises";
 import path from "node:path";
 import { spawnSync } from "child_process";
 
-await fs.writeFile(path.resolve(import.meta.dirname, "./package.json"), {
-  ...pkg,
-  main: "prepare.js",
-  bin: {
-    oxlint: "oxlint",
-  },
-});
+const __dirname = path.dirname(process.argv[1]);
 
-spawnSync("oxlint", process.argv.slice(2), {
+await fs.writeFile(
+  path.resolve(__dirname, "./package.json"),
+  JSON.stringify(
+    {
+      ...pkg,
+      main: "prepare.js",
+      bin: {
+        oxlint: "oxlint",
+      },
+    },
+    null,
+    2
+  )
+);
+
+const result = spawnSync("./oxlint", process.argv.slice(2), {
   shell: false,
   stdio: "inherit",
+  cwd: __dirname,
 });
+
+if (result.error) {
+  throw result.error;
+}
+
+process.exitCode = result.status;
